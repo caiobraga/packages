e775d2f879375b83d0cb077f03835758a9377ccf
Caio Gomes Braga <45905947+caiobraga@users.noreply.github.com>
2023-03-26 22:32:59 -0300

    atualização Date util

--- core/lib/utils/date_utils.dart
+++ core/lib/utils/date_utils.dart
@@ -1,8 +1,16 @@
+import '../types/timestamp.dart';
+
 class DateUtil {
   static final DateTime dateZero = DateTime(0);
 
   static DateTime toDateTimeDefaultDateZero(dynamic value) {
     if (value is DateTime) return value;
+
+    if(value is Timestamp ){
+      
+      final date = DateTime.fromMillisecondsSinceEpoch(value.microsecondsSinceEpoch);
+      return date;
+    }
     return DateTime.tryParse(value.toString()) ?? dateZero;
   }
 }
