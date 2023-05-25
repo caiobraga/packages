57047a9d6ca22ff49eef118d4bc2a5daa52d96c5
caiobraga <caiotagb@gmail.com>
2023-05-25 10:59:38 -0300

    widget appbar

--- design_system/lib/app_design_system.dart
+++ design_system/lib/app_design_system.dart
@@ -1,73 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-
 class AppDesignSystem {
   String appName;
   AppColors appColors;
   Fonts appFonts;
   AppImages appImages;
-  
 
-  AppDesignSystem({required this.appColors, required this.appFonts, required this.appImages, required this.appName});
+  AppDesignSystem(
+      {required this.appColors,
+      required this.appFonts,
+      required this.appImages,
+      required this.appName});
 }
 
 class AppColors {
   Color primaryColor;
-  Color secondary ;
-  Color tertiary ;
+  Color secondary;
+  Color tertiary;
 
   Color whiteColor;
   Color blackColor;
-  
 
-  Color textButtonColor ;
-  Color cardColor ;
-  Color iconColor ;
-  Color scaffoldBackground ;
+  Color textButtonColor;
+  Color cardColor;
+  Color iconColor;
+  Color scaffoldBackground;
 
-  AppColors({required this.primaryColor, required this.secondary, required this.tertiary, required this.whiteColor, required this.blackColor, required this.textButtonColor, required this.cardColor, required this.iconColor, required this.scaffoldBackground});
+  AppColors(
+      {required this.primaryColor,
+      required this.secondary,
+      required this.tertiary,
+      required this.whiteColor,
+      required this.blackColor,
+      required this.textButtonColor,
+      required this.cardColor,
+      required this.iconColor,
+      required this.scaffoldBackground});
 }
 
-class Fonts{
+class Fonts {
   double titleFont;
   double titleShortcuts;
   double subtitle;
   String? buttonTextFontFamily;
 
-  Fonts({required this.titleFont, required this.titleShortcuts, required this.subtitle, this.buttonTextFontFamily}); 
+  Fonts(
+      {required this.titleFont,
+      required this.titleShortcuts,
+      required this.subtitle,
+      this.buttonTextFontFamily});
 }
 
-class AppImages{
+class AppImages {
   String logoUrl;
 
   AppImages({required this.logoUrl});
 }
 
-
-
 class AppComponents {
-  static AppBar appBar(Widget title, BuildContext context, {bool hasIcon = true, bool automaticallyImplyLeading = false, IconButton? iconButton} ) {
+  static AppBar appBar(Widget title, BuildContext context,
+      {bool hasIcon = true,
+      bool automaticallyImplyLeading = false,
+      Widget? iconButton}) {
     final appDesign = Modular.get<AppDesignSystem>();
     return AppBar(
       automaticallyImplyLeading: automaticallyImplyLeading,
       centerTitle: true,
       backgroundColor: appDesign.appColors.primaryColor,
       elevation: 0,
-      leading: automaticallyImplyLeading != false ? BackButton(
-        color: appDesign.appColors.whiteColor,
-        onPressed: () {
-         Navigator.pop(context);
-        },
-      ) : Container(),
+      leading: automaticallyImplyLeading != false
+          ? BackButton(
+              color: appDesign.appColors.whiteColor,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          : Container(),
       title: title,
       actions: [
-        hasIcon?
-        iconButton!= null? iconButton : Container() :
-            Text(""),
+        hasIcon
+            ? iconButton != null
+                ? iconButton
+                : Container()
+            : Text(""),
       ],
       foregroundColor: appDesign.appColors.blackColor,
     );
   }
 }
-
